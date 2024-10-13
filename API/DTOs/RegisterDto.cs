e8d2c1b4245545666bb16ea1e6085c51c04755da
Jeron Rajak <jeronrajak@gmail.com>
2024-10-13 15:42:42 +0545

    error handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,12 @@ public class RegisterDto
 {
 
     [Required]
-    public required string Username {get; set;}
+    public string Username {get; set;} = string.Empty;
 
-    public required string Password {get; set; }
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+
+    public string Password {get; set; } = string.Empty;
 
 
 }
