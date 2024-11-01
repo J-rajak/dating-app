f4e97eab75d552047bd1f66ec4006cd9acaca3aa
Jeron Rajak <jeronrajak@gmail.com>
2024-11-01 18:17:02 +0545

    date picker added

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -20,11 +20,12 @@ import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { JsonPipe, NgIf } from '@angular/common';
 import { TextInputComponent } from '../_forms/text-input/text-input.component';
+import { DatePickerComponent } from '../_forms/date-picker/date-picker.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
