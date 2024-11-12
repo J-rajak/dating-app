ed4fb984c7bccb859cb6d59aafb19319041b0d63
Jeron Rajak <jeronrajak@gmail.com>
2024-11-12 16:58:14 +0545

    filtering with gender included and also paging

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -5,15 +5,19 @@ namespace API.Helpers;
 public class UserParams
 {
 
-    private const int MaxPageSize =  50;
+    private const int MaxPageSize = 50;
 
     public int PageNumber { get; set; } = 1;
 
     private int _pageSize = 10;
 
-    public int PageSize {
+    public int PageSize
+    {
         get => _pageSize;
         set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
     }
 
+    public string? Gender { get; set; }
+
+    public string? CurrentUsername { get; set; }
 }
