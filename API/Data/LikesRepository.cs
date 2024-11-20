952d1458e75243dd0682187abc2c7d7f61d32af8
Jeron Rajak <jeronrajak@gmail.com>
2024-11-20 14:43:22 +0545

    changes in likes repository

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data;
 
 public class LikesRepository(DataContext context, IMapper mapper) : ILikesRepository
+
 {
     public void AddLike(UserLike like)
     {
