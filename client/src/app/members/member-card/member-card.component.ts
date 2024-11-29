681ec6d8a960be952ef331b2f46eaba2a1e9bb79
Jeron Rajak <jeronrajak@gmail.com>
2024-11-29 12:17:57 +0545

    implemented like apis in the client side

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,14 +1,26 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
+import { LikesService } from '../../_services/likes.service';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
 })
 export class MemberCardComponent {
+  private likeService = inject(LikesService);
   member = input.required<Member>();
+
+  hasLiked = computed(() =>
+    this.likeService.likeIds().includes(this.member().id)
+  );
 }
