c6d71060e37127e4c17ab4e62a1db1dd00e2b253
Jeron Rajak <jeronrajak@gmail.com>
2025-02-07 09:05:49 +0545

    changes in member card component

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -24,7 +24,6 @@ export class MemberCardComponent {
     this.likeService.likeIds().includes(this.member().id)
   );
 
-  
   toggleLike() {
     this.likeService.toggleLike(this.member().id).subscribe({
       next: () => {
