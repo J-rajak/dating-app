f4320f4d6a4358d1d981ebd98fb2a64a49e1eff9
Jeron Rajak <jeronrajak@gmail.com>
2025-04-10 07:26:26 +0545

    changes in likes services

--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -36,6 +36,7 @@ export class LikesService {
       });
   }
 
+  
   getLikesIds() {
     return this.http.get<number[]>(`${this.baseUrl}likes/list`).subscribe({
       next: (ids) => this.likeIds.set(ids),
