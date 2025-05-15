71e5e62b49a2512dacd6f0f33467487aafbfbfc1
Jeron Rajak <jeronrajak@gmail.com>
2025-05-15 19:01:05 +0545

    changes in likes controller

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+
 public class LikesController(ILikesRepository likesRepository) : BaseApiController
 {
     [HttpPost("{targetUserId:int}")]
