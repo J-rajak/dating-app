f923144990a19a8d90d8f53612f873044a65d640
Jeron Rajak <jeronrajak@gmail.com>
2025-05-16 21:36:40 +0545

    changes in weather controller

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -5,6 +5,7 @@ namespace API.Controllers;
 
 public class WeatherForecastController : BaseApiController
 {
+    
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
