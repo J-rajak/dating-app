0a948741e2de9ecbe08d6e87892441164281e8ae
Jeron Rajak <jeronrajak@gmail.com>
2025-05-19 08:21:25 +0545

    changes in data context

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,7 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
 
+
     public DbSet<AppUser> Users { get; set; }
 
     public DbSet<UserLike> Likes { get; set; }
