23ebc7388dde26acf9571ce66aeabbde7731b3bc
Jeron Rajak <jeronrajak@gmail.com>
2025-05-31 08:18:33 +0545

    changes in buggy

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-
 public class BuggyController(DataContext context) : BaseApiController
 {
     [Authorize]
