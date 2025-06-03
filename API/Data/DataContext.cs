dc0dc8076a17cab0be860b4c76d4e616bc7603a0
Jeron Rajak <jeronrajak@gmail.com>
2025-06-03 08:02:18 +0545

    changes in data context

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -5,8 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data;
 
 public class DataContext(DbContextOptions options) : DbContext(options)
-{
-    public DbSet<AppUser> Users { get; set; }
+{    public DbSet<AppUser> Users { get; set; }
 
     public DbSet<UserLike> Likes { get; set; }
 
